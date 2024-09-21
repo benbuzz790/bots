ad09baf33f07a410e45e2247fce8d27a6c2b5e22
benbuzz790 <benbuzz790@gmail.com>
2024-09-21 11:17:35 -0600

    correct import issue #19

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 ﻿from setuptools import setup, find_packages
+import os
+
+# Read the long description from README.md
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='bots',
     version='0.1',
     author='Ben Rinauto',
     author_email='ben.rinauto@gmail.com',
-    description='A framework for llm tool use',
-    long_description=open('README.md').read(),
+    description='A framework for LLM tool use',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/benbuzz790/bots',
     classifiers=[
@@ -14,8 +19,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    package_dir={'': 'bots'},  # Adjust this to point to the new 'bots' directory
-    packages=find_packages(where='bots'),  # Automatically find packages in 'bots'
+    packages=find_packages(),  # Automatically find all packages including 'bots' and its sub-packages
     install_requires=[
         'anthropic',
         'openai',
@@ -25,4 +29,5 @@ setup(
     extras_require={
         'dev': ['pytest'],
     },
+    python_requires='>=3.6',  # Specify Python version compatibility
 )
