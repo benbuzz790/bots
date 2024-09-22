bf815b3d5ca853b0cdc527ac35f3dfb847025b7d
benbuzz790 <benbuzz790@gmail.com>
2024-09-22 16:15:00 -0600

    feat github tools

--- bots/__init__.py
+++ bots/__init__.py
@@ -4,8 +4,8 @@ from .foundation.openai_bots import ChatGPT_Bot
 
 # Import entire modules
 from . import auto_terminal
-from . import lazy
-from .tools import python_tools
+from .lazy import lazy
+from .tools import python_tools, github_tools
 from .foundation.base import Engines
 
 __all__ = [
@@ -14,5 +14,6 @@ __all__ = [
     'auto_terminal',
     'lazy',
     'python_tools',
+    'github_tools',
     'Engines'
 ]
