496c2e7736fc5ad2478f551d454e54c3b7b3317f
benbuzz790 <benbuzz790@gmail.com>
2024-12-24 16:51:19 -0700

    better

--- examples/functional_prompt_example.py
+++ examples/functional_prompt_example.py
@@ -1,8 +1,12 @@
+# A simple example showing a chain prompt and a branch prompt
+
 import bots
 import bots.flows.functional_prompts as fp
 
-bot = bots.AnthropicBot()
-prompt_chain = [
+bot1 = bots.AnthropicBot(name = "chain")
+bot2 = bots.AnthropicBot(name = "branch")
+
+prompt_set = [
     "Hello! How are you today?",
     "What's the weather like up there? Get it, because you're tall? Haha.",
     "Intangible, eh? That's pretty hot...",
@@ -10,6 +14,8 @@ prompt_chain = [
     "Yes, agreed. What do you know about... biology?",
     "You're so stuffy. A wet rag. A debbie downer. I was hoping you knew a little about... the heart."
 ]
-fp.chain(bot, prompt_chain)
 
-print(bot)
+fp.chain(bot1, prompt_set)
+print(bot1)
+fp.branch(bot2, prompt_set)
+print(bot2)
