dd270184e310421f6158e3f842cc9bc48e26767b
benbuzz790 <benbuzz790@gmail.com>
2025-04-01 12:11:20 -0600

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -56,3 +56,26 @@ language = 'en'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# These paths are either relative to html_static_path or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/theme.css',
+]
+
+# If true, links to the reST sources are added to the pages.
+html_show_sourcelink = False
+
+# Theme options
+html_theme_options = {
+    'canonical_url': 'https://benbuzz790.github.io/bots/',
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': '#2980B9',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
