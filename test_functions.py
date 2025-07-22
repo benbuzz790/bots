b9fa463fe788a08b837c9f140d900d156279b789
Ben Rinauto <ben.rinauto@gmail.com>
2025-07-22 17:56:50 -0600

    delete test files

--- test_functions.py
+++
@@ -1,9 +0,0 @@
-def func1():
-    return "original func1"
-
-def func2():
-    return "replaced func2"
-    # with extra comment
-
-def func3():
-    return "original func3"
--- test_insert.py
+++
@@ -1,8 +0,0 @@
-class TestInsert:
-    def method1(self):
-        return "method1"
-    def method2(self):
-        return "method2 inserted"
-
-    def method3(self):
-        return "method3"
