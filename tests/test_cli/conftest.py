e488cf42d343bed9eaeaf98a9b62270a2eaef8e8
Ben Rinauto <ben.rinauto@gmail.com>
2025-10-01 23:40:16 -0600

    make some tests serial

---
+++ tests/test_cli/conftest.py
@@ -0,0 +1,5 @@
+﻿import pytest
+def pytest_collection_modifyitems(items):
+    '''Mark all tests in test_cli directory to run serially in same group.'''
+    for item in items:
+        item.add_marker(pytest.mark.xdist_group('cli_serial'))
---
+++ tests/test_powershell_tool/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+def pytest_collection_modifyitems(items):
+    '''Mark all tests in test_powershell_tool directory to run serially in same group.'''
+    for item in items:
+        item.add_marker(pytest.mark.xdist_group('terminal_serial'))
